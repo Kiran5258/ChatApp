52991e3643b23bd89f32eb3266a9272db226b59a
Kiran5258 <skirankumar97849@gmail.com>
2026-03-11 18:49:12 +0530

    Prepare for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,39 @@
 import express from "express";
 import dotenv from 'dotenv'
 import cors from 'cors';
+import path from "path";
 import authRoutes from "../src/routes/auth.route.js";
 import messageRoute from "../src/routes/message.route.js"
 import { connectDb } from "./config/db.js";
 import cookieParser from "cookie-parser";
-import {app,server}from "./config/socket.js";
+import { app, server } from "./config/socket.js";
+
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser())
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
+    origin: process.env.NODE_ENV === "development" ? "http://localhost:5173" : process.env.FRONTEND_URL,
+    credentials: true,
 }))
 
-const PORT =process.env.PORT;
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoute);
 
-server.listen(PORT,()=>{console.log(`http://localhost:${PORT}`)
-connectDb();
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    connectDb();
+})
